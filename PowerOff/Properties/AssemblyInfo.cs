215c33f079fcf759924a58e5ddbb3a6d785bbd28
Cr0Nos <cronosjkeee@gmail.com>
2016-07-24 16:45:45 +0800

    commitpo

--- PowerOff/Properties/AssemblyInfo.cs
+++ PowerOff/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PowerOff")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Cr0Nos 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
